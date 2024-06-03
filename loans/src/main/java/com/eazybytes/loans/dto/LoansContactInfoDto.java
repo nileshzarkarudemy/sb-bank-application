bdf51f90c1a1805aa68c0527908cbcea7bf558ee
nileshzarkar <nileshzarkar@gmail.com>
2024-06-04 00:51:16 +0530

    added refresh and busrefresh

--- loans/src/main/java/com/eazybytes/loans/dto/LoansContactInfoDto.java
+++ loans/src/main/java/com/eazybytes/loans/dto/LoansContactInfoDto.java
@@ -4,7 +4,15 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Getter;
+import lombok.Setter;
 @ConfigurationProperties(prefix = "loans")
-public record LoansContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter @Setter
+public class LoansContactInfoDto {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 
 }
