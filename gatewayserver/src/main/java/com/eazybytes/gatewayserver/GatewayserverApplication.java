7662ee0180937d3dffa71c42bc1ac2e5b131deba
nileshzarkar <nileshzarkar@gmail.com>
2024-06-21 21:11:36 +0530

    added rate limitter part 1

--- gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -1,21 +1,22 @@
 package com.eazybytes.gatewayserver;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.concurrent.TimeoutException;
-
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
-import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
-import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 
 @SpringBootApplication
@@ -26,7 +27,6 @@ public class GatewayserverApplication {
 	}
 
 
-	@SuppressWarnings("unchecked")
 	@Bean
 	public RouteLocator eazyBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder){
 		return routeLocatorBuilder.routes()
@@ -46,7 +46,8 @@ public class GatewayserverApplication {
 				.uri("lb://LOANS"))
 				.route(p -> p.path("/eazybank/cards/**")
 				.filters(f -> f.rewritePath("/eazybank/cards/(?<segment>.*)", "/${segment}")
-				               .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))				
+				               .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+							   .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter()).setKeyResolver(userKeyResolver())))				
 				.uri("lb://CARDS"))
 				.build();
 	}
@@ -58,4 +59,14 @@ public class GatewayserverApplication {
 				.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
 	}
 
+	@Bean
+	public RedisRateLimiter redisRateLimiter() {
+		return new RedisRateLimiter(1, 1, 1);
+	}
+
+	@Bean
+	KeyResolver userKeyResolver() {
+		return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("user"))
+				.defaultIfEmpty("anonymous");
+	}
 }
