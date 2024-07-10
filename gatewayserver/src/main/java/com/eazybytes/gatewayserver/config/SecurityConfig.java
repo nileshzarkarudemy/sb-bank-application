dadb34dca0f3712c6da8b4647cb0f895a832f487
nileshzarkar <nileshzarkar@gmail.com>
2024-07-10 19:16:01 +0530

    demo of client authentication

--- gatewayserver/src/main/java/com/eazybytes/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/eazybytes/gatewayserver/config/SecurityConfig.java
@@ -45,9 +45,5 @@ public class SecurityConfig {
         return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
     }
 
-    @Bean
-    public ReactiveJwtDecoder jwtDecoder() {
-        return NimbusReactiveJwtDecoder.withJwkSetUri("http://localhost:7080/realms/master/protocol/openid-connect/certs").build();
-    }
-
+  
 }
