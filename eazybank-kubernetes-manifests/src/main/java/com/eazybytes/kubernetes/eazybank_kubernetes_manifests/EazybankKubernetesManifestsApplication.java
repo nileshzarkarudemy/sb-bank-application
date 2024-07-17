1100a3a13c100a27ea25d51fb322d6b41fe2d6cf
nileshzarkar <nileshzarkar@gmail.com>
2024-07-17 10:14:26 +0530

    running the microservices inside a kubernetes cluster

---
+++ eazybank-kubernetes-manifests/src/main/java/com/eazybytes/kubernetes/eazybank_kubernetes_manifests/EazybankKubernetesManifestsApplication.java
@@ -0,0 +1,13 @@
+package com.eazybytes.kubernetes.eazybank_kubernetes_manifests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazybankKubernetesManifestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazybankKubernetesManifestsApplication.class, args);
+	}
+
+}
---
+++ eazybank-kubernetes-manifests/src/test/java/com/eazybytes/kubernetes/eazybank_kubernetes_manifests/EazybankKubernetesManifestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.kubernetes.eazybank_kubernetes_manifests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazybankKubernetesManifestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
